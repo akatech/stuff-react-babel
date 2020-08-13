f331c62e4f3aced5e3d7ff25cada796c5c77c529
akatech04 <cissokho.abdou-karim1@ugb.edu.sn>
2020-08-13 20:23:00 +0000

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+/* const button = React.createElement("button", { disabled: true }, 'My button');
+
+ReactDOM.render(button, document.getElementById("root")) */
+
+let foo = "bar"
+
+console.log(foo)
